4d6cf38b266f32cb3cc5fe7f8f36f6c1743fc610
Gabriellocp <gabriel_campista@hotmail.com>
2025-03-28 17:03:19 -0300

    chore: Add env validation

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { env } from 'src/shared/config/env';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'unsecure_jwt_secret',
+      secret: env.jwtSecret,
       signOptions: { expiresIn: '7d' }
     })
   ],
